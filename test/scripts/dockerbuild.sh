cdc00161dbadf8c5668fe1a2a2795246a0d0fb0c
Naveen.sk <naveensk@V-labs.ai>
2023-02-09 23:30:44 +0530

    docker fil added

--- test/scripts/dockerbuild.sh
+++ test/scripts/dockerbuild.sh
@@ -1,26 +1,11 @@
 #!/bin/sh
 
-ls
-cd my-app-repo
-cd react-app/
-
-ls
-
-
-
-# move react build file into nginx directory.....
-
-# cp -R ./build /
-
-# cd /
-
-# ls
 
 
 
 # build the Docker image
-docker build -t my-docker-image .
+docker build -t reactapp-nginx my-app-repo/test/Docker/.
 
 
   # push the Docker image
-docker push my-docker-image         
+docker push navin621/test/reactapp-nginx:latest       
