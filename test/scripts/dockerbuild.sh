f4c764e3db4a14b0da1577aa70afef3af978e219
Naveen.sk <naveensk@V-labs.ai>
2023-02-09 23:39:26 +0530

    dd

--- test/scripts/dockerbuild.sh
+++ test/scripts/dockerbuild.sh
@@ -3,9 +3,10 @@
 
 
 
-# build the Docker image
-docker build -t reactapp-nginx my-app-repo/test/Docker/.
+dokcer 
+# # build the Docker image
+# docker build -t reactapp-nginx my-app-repo/test/Docker/.
 
 
-  # push the Docker image
-docker push navin621/test/reactapp-nginx:latest       
+#   # push the Docker image
+# docker push navin621/test/reactapp-nginx:latest       
