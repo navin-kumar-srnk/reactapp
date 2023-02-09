dfb07d141275c657c50b5965ea5d0e603576b044
Naveen.sk <naveensk@V-labs.ai>
2023-02-09 12:44:46 +0530

    pp

--- test/scripts/dockerbuild.sh
+++ test/scripts/dockerbuild.sh
@@ -13,7 +13,7 @@ ls
 # move react build file into nginx directory
 
 
-COPY my-app-repo/react-app/build /usr/share/nginx/html
+cp my-app-repo/react-app/build/. /usr/share/nginx/html
 
 
 cd /usr/share/nginx/html
